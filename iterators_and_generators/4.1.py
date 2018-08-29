10f8bb5668004987980095c60526f2d602170902
吴文迪 <vendywu@haoqiao.com>
2018-08-29 16:23:57 +0800

    next(f, None)

--- iterators_and_generators/4.1.py
+++ iterators_and_generators/4.1.py
@@ -1,8 +1,7 @@
 # 不用for循环来遍历一个序列中的每一个元素
 with open('/etc/passwd') as f:
-    try:
-        while True:
-            line = next(f)
-            print(line, end='')
-    except StopIteration:
-        pass
+    while True:
+        line = next(f, None)
+        if line is None:
+            break
+        print(line, end="")
