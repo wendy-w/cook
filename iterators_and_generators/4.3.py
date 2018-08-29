fedce59dcdbc2f2be5ee2fe9dbeb142766010535
吴文迪 <vendywu@haoqiao.com>
2018-08-29 17:01:41 +0800

    genarator and yeild

---
+++ iterators_and_generators/4.3.py
@@ -0,0 +1,11 @@
+def frange(start, stop, increment):
+    x = start
+    while x < stop:
+        yield x
+        x += increment
+
+
+for n in frange(0, 4, 0.5):
+    print(n)
+
+print(list(frange(1, 10, 2)))
