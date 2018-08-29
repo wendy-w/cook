e0a91c1a0f89d8dd24e4b086a5dcebb8feb02f94
吴文迪 <vendywu@haoqiao.com>
2018-08-29 17:00:45 +0800

    genarator and yeild

--- iterators_and_generators/4.1.py
+++ iterators_and_generators/4.1.py
@@ -1,5 +1,11 @@
 # 不用for循环来遍历一个序列中的每一个元素
 with open('/etc/passwd') as f:
+    # try:
+    #     while True:
+    #         line = next(f)
+    #         print(line, end='')
+    # except StopIteration:
+    #     pass
     while True:
         line = next(f, None)
         if line is None:
